97015cf219fd7994f6aeb3e30c792b687fbabe3f
gc986 <yazovcev@netlabsystems.com>
2020-02-21 12:20:10 +0500

    First commit

---
+++ app/src/main/java/ru/gc986/sqlitetest/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.gc986.sqlitetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
